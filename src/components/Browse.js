f2e8da6f206e9154a1f77c5f67bf96c7a555db37
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2024-03-20 00:31:56 +0530

    TMDB API data Fetch && built MainContainer

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import Header from './Header'
+import Header from "./Header"
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer"
+import SecondaryContainer from "./SecondaryContainer"
 
 const Browse = () => {
+  useNowPlayingMovies();
+
   return (
     <div>
-      <Header/>
-      <div className='absolute'>
-      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/a3873901-5b7c-46eb-b9fa-12fea5197bd3/IN-en-20240311-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="bg" />
-      </div>
+      <Header />
+      <MainContainer />
+      <SecondaryContainer />
     </div>
   )
 }
