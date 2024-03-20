c4ca65f7aa182268ed361e6530a62f91bcd86d01
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2024-03-20 22:07:59 +0530

    Secondary Container with movie list && usePopularMovies,useTopRatedMovies,useUpcomingMovies

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,10 +2,12 @@ export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897
 
 export const USER_AVATAR = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
 
-export const API_OPTIONS =  {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGRhYjEzZDZhNDhlOWNiNWM3ZDhkMGNhYmE0OTUwYiIsInN1YiI6IjY1ZjkwZTQyYWJkZWMwMDE4NjZhZTdmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RRvVTeMkpL5xtH6BIuv0pHF7R-tEIKN_NOAy9o1mSx4'
-    }
-  };
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGRhYjEzZDZhNDhlOWNiNWM3ZDhkMGNhYmE0OTUwYiIsInN1YiI6IjY1ZjkwZTQyYWJkZWMwMDE4NjZhZTdmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RRvVTeMkpL5xtH6BIuv0pHF7R-tEIKN_NOAy9o1mSx4'
+  }
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
